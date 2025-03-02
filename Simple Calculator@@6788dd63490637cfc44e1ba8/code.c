19355a1e7f45482540c74fac35675841be58bbec
Gurleen Saini <196659986+gurleen1166@users.noreply.github.com>
2025-03-02 11:52:06 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -12,17 +12,17 @@ int main() {
 
     switch (opr) {
         case '+':
-            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            printf("%d + %d = %d\n", num1 + num2);
             break;
         case '-':
-            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            printf("%d - %d = %d\n", num1 - num2);
             break;
         case '*':
-            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            printf("%d * %d = %d\n", num1 * num2);
             break;
         case '/':
             if (num2 != 0) {
-                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+                printf("%d / %d = %d\n", num1 / num2);
             } else {
                 printf("Error! Division by zero.\n");
             }
