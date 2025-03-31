24344dd7d904b106e6ecf3772b02c08ebc2e1ad7
Gurleen Saini <196659986+gurleen1166@users.noreply.github.com>
2025-03-31 21:50:50 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include<stdio.h>
+void prime(int arr[],int n){
+    for(int i=0;i<n;i++){
+       
+     if(arr[i]%2==0){
+        printf("0");
+     }
+     else{
+        printf("1");
+     }
+        
+    }
+}
