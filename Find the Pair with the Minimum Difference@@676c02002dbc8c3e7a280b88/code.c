964468a183fd7e444a19599e0f3fe0dc1ff9479d
Gurleen Saini <196659986+gurleen1166@users.noreply.github.com>
2025-04-08 23:27:09 +0530

    Solution to Find the Pair with the Minimum Difference

---
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -0,0 +1,40 @@
+// Your code here...
+#include <stdio.h>
+#include <stdlib.h>  // for abs()
+
+void findMinDiffPair(int arr[], int size) {
+    if (size < 2) {
+        return;
+    }
+
+    int minDiff = abs(arr[0] - arr[1]);
+    int minPair1 = arr[0], minPair2 = arr[1];
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            int diff = abs(arr[i] - arr[j]);
+
+            if (diff < minDiff) {
+                minDiff = diff;
+                minPair1 = arr[i];
+                minPair2 = arr[j];
+            }
+        }
+    }
+
+  
+}
+
+int main() {
+    int size;
+    scanf("%d", &size);
+
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    findMinDiffPair(arr, size);
+
+    return 0;
+}
