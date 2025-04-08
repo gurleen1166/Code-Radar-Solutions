be68fa76a83c2fc34c96c9081f4bdadd0ef70302
Gurleen Saini <196659986+gurleen1166@users.noreply.github.com>
2025-04-09 00:11:34 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Simple bubble sort
 void sortArray(int arr[], int size) {
     for (int i = 0; i < size-1; i++) {
         for (int j = 0; j < size-1-i; j++) {
             if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
+                int tmp = arr[j];
                 arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j+1] = tmp;
             }
         }
     }
 }
 
-// Function to get absolute difference without using abs()
-int getDiff(int a, int b) {
-    return (a > b) ? (a - b) : (b - a);
-}
-
 int main() {
     int size;
     scanf("%d", &size);
-
-    if (size<=1) {
+    if (size < 2) {
+        
         return -1;
     }
 
@@ -31,22 +26,22 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
+    // Step 1: Sort array
     sortArray(arr, size);
 
-    int minDiff = getDiff(arr[0], arr[1]);
+    // Step 2: Find minimum difference between adjacent sorted elements
+    int minDiff = abs(arr[1] - arr[0]);
     int a = arr[0], b = arr[1];
 
     for (int i = 1; i < size - 1; i++) {
-        int diff = getDiff(arr[i], arr[i + 1]);
+        int diff = abs(arr[i+1] - arr[i]);
         if (diff < minDiff) {
             minDiff = diff;
             a = arr[i];
-            b = arr[i + 1];
+            b = arr[i+1];
         }
     }
 
-    // Already sorted, so a < b guaranteed
     printf("%d %d\n", a, b);
-
     return 0;
 }
