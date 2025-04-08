370307f7f2f1eee00a43acc3d61ff33d8f4af337
Gurleen Saini <196659986+gurleen1166@users.noreply.github.com>
2025-04-08 23:01:43 +0530

    Solution to Find the Leaders in an Array

---
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -0,0 +1,26 @@
+// Your code here...
+#include <stdio.h>
+
+void findLeadersBruteForce(int arr[], int size) {     
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] <= arr[j]) {
+                break;
+            }
+        }
+        if (j == size) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    int arr[];
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    findLeadersBruteForce(arr, size);
+
+    return 0;
+}
